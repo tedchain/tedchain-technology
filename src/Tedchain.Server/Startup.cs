e6989d9a6a3172b19496db6ac80030588ee9705a
HOWARD LARRISON <development@daybreak.coffee>
2018-06-07 15:42:27 +0700

    Update Startup.cs

--- src/Tedchain.Server/Startup.cs
+++ src/Tedchain.Server/Startup.cs
@@ -1,6 +1,6 @@
 ﻿// Copyright (c) 2010-2018 The Bitcoin developers
 // Original code was distributed under the MIT software license.
-// Copyright (c) 2014-2018 TEDLab Sciences Ltd
+// Copyright (c) 2015-2018 TEDLab Sciences Ltd
 // Tedchain code distributed under the GPLv3 license, see COPYING file.
 
 using System.Collections.Generic;
