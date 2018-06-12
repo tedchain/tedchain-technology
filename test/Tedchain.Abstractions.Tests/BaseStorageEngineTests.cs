8c8195256cc973e43da5289f21557c5b268d975b
HOWARD LARRISON <development@daybreak.coffee>
2018-06-13 00:54:17 +0700

    Update BaseStorageEngineTests.cs

--- test/Tedchain.Abstractions.Tests/BaseStorageEngineTests.cs
+++ test/Tedchain.Abstractions.Tests/BaseStorageEngineTests.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) 2010-2018 The Bitcoin developers
+﻿// Copyright (c) 2010-2018 The Bitcoin developers.
 // Original code was distributed under the MIT software license.
 // Copyright (c) 2014-2018 TEDLab Sciences Ltd
 // Tedchain code distributed under the GPLv3 license, see COPYING file.
