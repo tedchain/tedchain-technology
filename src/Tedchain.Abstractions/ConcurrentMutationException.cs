3b7ca632d8938ecf67a6bda5793504b343dc3503
HOWARD LARRISON <development@daybreak.coffee>
2018-06-23 15:02:40 +0700

    Update ConcurrentMutationException.cs

--- src/Tedchain.Abstractions/ConcurrentMutationException.cs
+++ src/Tedchain.Abstractions/ConcurrentMutationException.cs
@@ -1,5 +1,5 @@
 ﻿// Copyright (c) 2010-2018 The Bitcoin developers
-// Original code was distributed under the MIT software license.
+// Original code was distributed under the MIT software license
 // Copyright (c) 2014-2018 TEDLab Sciences Ltd
 // Tedchain code distributed under the GPLv3 license, see COPYING file.
 
